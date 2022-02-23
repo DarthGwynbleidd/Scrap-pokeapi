48b432c46f6b9b5eccee4006c4178098f5a1e21b
unknown <gaeldouence@gmail.com>
2022-02-23 23:22:08 +0100

    add feature scrap total number of pokemon to limit loop + add congrat ans defeat msg at the end

--- scrap-pokemon/scrap-pokemon.js
+++ scrap-pokemon/scrap-pokemon.js
@@ -3,29 +3,44 @@ const obj = {}
 let fs = require('fs');
 async function fetchPokemons() {
 
-    for (i=1; i <= 898 ; i++){
- 
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
-        
+    const responseToLength = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/`)
+    const totalPokemons = responseToLength.data.count
+
+    for (number = 1; number <= totalPokemons; number++) {
+
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${number}`)
+
         let french = ''
-        let english = ''
-        for (const index of response.data.names){
+        let english = response.data.name
+        for (const index of response.data.names) {
             if (index.language.name === 'fr')
                 french = index.name
-            else if (index.language.name === 'en')
-                english = index.name
         }
         obj[french] = english
-        console.log(french)
-        console.log(english)
+        console.log(" ");
+        console.log(`Pokemon № ${number} -- French: ${french} ===> English: ${english}`)
+        console.log(" ");
+        console.log('-----------------------------')
     }
-    
+
 }
 fetchPokemons()
 
-setTimeout(() => { fs.writeFile ("pokemon_translate.json", JSON.stringify(obj), function(err) {
-    if (err) throw err;
-    console.log(Object.keys(obj).length);
+setTimeout(() => {
+    fs.writeFile("pokemon_translate.json", JSON.stringify(obj), function (err) {
+        if (err) throw err;
+        if (totalPokemons === Object.keys(obj).length) {
+            console.log(`================`);
+            console.log(` `);
+            console.log(` <---- Congratulations, you have just obtained the complete list of pokemons ---->`);
+            console.log(` `);
+            console.log(`================`);
+        }
+        else {
+            console.log(` `);
+            console.log(`/!\\ unfortunately an error has occurred somewhere, you have not retrieved the complete list of pokemons /!\\`);
+            console.log(` `);
+        }
     }
-);
-  }, 200000)
+    );
+}, 50000)
